2dea1a2fc0f35935996a791b9d27a3db04f2b34f
GuaiMiu <1297685880@qq.com>
2023-08-22 20:54:05 +0800

    已完成腾讯云阿里云的DDNS

--- src/components/ssl.vue
+++ src/components/ssl.vue
@@ -2,14 +2,49 @@
     <div class="ssl">
         <div class="ssl-header">
             <h1>SSL</h1>
-            <el-button type="primary"  style="margin-left: auto;margin-right: 10px;">新建配置</el-button>
+            <el-button type="primary" style="margin-left: auto;margin-right: 10px;" @click="openNewConfig">新建配置</el-button>
         </div>
+        <el-dialog v-model="isConfigDialog" title="新建配置">
+
+
+        </el-dialog>
+        <div class="ssl-body">
+
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column prop="ID" label="配置名" width="120" />
+                <el-table-column prop="CREATED_TIME" label="创建时间" width="180" />
+                <el-table-column prop="DOMAIN" label="域名" width="180" />
+                <el-table-column prop="DNS" label="服务商" width="180" />
+                <el-table-column prop="STATUS" label="状态" />
+                <el-table-column fixed="right" label="操作" width="120">
+                    <template #default="scope">
+                        <el-button link type="primary" size="small" @click="handleClick(scope.$index)">编辑</el-button>
+                        <el-popconfirm title="确定删除配置？?" confirm-button-text="Yes" cancel-button-text="No" :icon="InfoFilled"
+                            icon-color="#626AEF" @confirm="deleteConfig(scope.$index)">
+                            <template #reference>
+                                <el-button link type="primary" size="small">删除</el-button>
+                            </template>
+                        </el-popconfirm>
+
+                    </template>
+                </el-table-column>
+            </el-table>
+
+
+
+        </div>
+
     </div>
 </template>
     
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-
+//新建配置Dialog 默认隐藏
+const isConfigDialog = ref(false)x
+//打开dialog
+const openNewConfig = () => {
+    isConfigDialog.value = true
+}
 </script>
     
 <style scoped>
@@ -24,4 +59,11 @@ import { ref, reactive } from 'vue'
     width: 100%;
     align-items: center;
 }
+
+
+.ssl .ssl-body {
+    width: 100%;
+    border: 1px solid #ccc;
+}
+
 </style>
